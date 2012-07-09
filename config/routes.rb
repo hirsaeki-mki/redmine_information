e3fbea5f4e4476534af0ae18f935ef1a30bd1ce8
myoshida <myoshida@0ebdb382-be1d-4e49-b691-f6e2a62a90dc>
2012-07-09 13:11:40 +0000

    refs #1079 : Adds a routing file, fixes an internal error of a setting page.
    git-svn-id: http://svn.osdn.jp/svnroot/rp-information/trunk/redmine_information@45 0ebdb382-be1d-4e49-b691-f6e2a62a90dc

---
+++ config/routes.rb
@@ -0,0 +1,2 @@
+
+match '/info/:action', :to=>'info'
--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_information do
   name 'Redmine Information Plugin'
   author 'M. Yoshida'
   description 'This is a plugin for information of Redmine'
-  version '1.0.0'
+  version '1.0.1(beta)'
   url 'http://www.r-labs.org/projects/rp-admin-reports/wiki/Redmine_Information_Plugin'
   author_url 'http://yohshiy.blog.fc2.com/'
 
